2e703928be0c0c26e6a2bc60f2ce22bff21e21a4
BorisBorisov33 <bobbyborisov16@gmail.com>
2022-11-18 08:51:41 +0200

    update of the project

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ import requests
 api_url = "https://jsonplaceholder.typicode.com/users"
 response = requests.get(api_url)
 print(response.json())
-
-# here I am uploading an image
+#
+# # here I am uploading an image
 images = {'image': open('C:/Users/as/Desktop/REST-API-python/test_image.png', 'rb')}
 requests.post(api_url, files=images)
+
+# get and filter images with specific longitude and latitude
+
+api_url = "https://jsonplaceholder.typicode.com/users?filter=lat&lng=-37.3159,81.1496"
+response = requests.get(api_url)
+print(response.json())
